68bc607bb3281a73a7ce3c728b155f6da1ab37b3
julioct99 <jcasallot01@gmail.com>
2021-05-25 20:12:55 +0200

    EC2 User data 1.0

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-APP_FOLDER=~/flask-test-2
+# Use this as the user data when launching a EC2 instance
+APP_FOLDER=/home/ubuntu/flask-test-2
 NGINX_CONF=/etc/nginx/sites-enabled/flaskapp.conf
+HOST=$(curl checkip.amazonaws.com)
+echo export HOST=$(curl checkip.amazonaws.com) >> /etc/profile
 
-git clone https://github.com/julioct99/flask-test-2.git
+su ubuntu
+git clone https://github.com/julioct99/flask-test-2.git /home/ubuntu/flask-test-2
 
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install python3-pip -y
 pip3 install -r $APP_FOLDER/requirements.txt 
-chmod +x $APP_FOLDER/*.sh
-
 sudo apt install gunicorn -y
 sudo apt-get install nginx -y
+chmod +x $APP_FOLDER/*.sh
 
 sudo cp $APP_FOLDER/nginx/flaskapp.conf $NGINX_CONF
 sudo sed -i "s/__HOST/${HOST}/g" $NGINX_CONF
